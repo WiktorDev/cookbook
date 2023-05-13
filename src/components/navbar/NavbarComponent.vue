ed64d43a0b08b55a30b630fdb39f8f75ff098039
Wiktor Wilk <mcserw4@wp.pl>
2023-05-13 12:08:01 +0200

    navbar component

---
+++ src/components/navbar/NavbarComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav class="flex justify-between p-2 items-center h-24 px-10">
+    <div>
+      <h1 class="text-2xl font-bold">LOGO</h1>
+    </div>
+    <div @click="open">
+      <h1 class="text-2xl font-bold">H</h1>
+    </div>
+  </nav>
+  <div v-if="isOpen" class="fixed top-0 left-0 w-full h-full z-10 bg-white transition-all duration-300 ease-in-out">
+		<div class="absolute z-20 w-full flex justify-end p-10" @click="open">
+			X
+		</div>
+    <ul class="h-screen flex flex-col items-center justify-center">
+			<NavbarLink name="Wszystkie przepisy" link="/r"/>
+      <NavbarLink name="Ulubione przepisy" link="/r"/>
+      <NavbarLink name="Dodaj przepis" link="/r"/>
+    </ul>
+  </div>
+</template>
+<script setup>
+import NavbarLink from "@/components/navbar/NavbarLink.vue";
+import {ref} from "vue";
+
+const isOpen = ref(false)
+function open() {
+	isOpen.value = !isOpen.value
+}
+</script>
---
+++ src/components/navbar/NavbarLink.vue
@@ -0,0 +1,12 @@
+<template>
+  <li class="text-2xl font-semibold mb-8 group transition-all duration-300 ease-in-out">
+    <router-link class="bg-left-bottom bg-gradient-to-r from-[#D4A373] to-[#D4A373] bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out" :to="link">
+      {{ name }}
+    </router-link>
+  </li>
+</template>
+<script>
+export default {
+	props: ['name', 'link']
+}
+</script>
